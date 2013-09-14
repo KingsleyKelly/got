ffcaa8b167626927fec9d57cd7cf4d2d456dcf7e
Kingsley Kelly <kk@converser.io>
2013-09-14 22:11:44 +0100

    updated the script to do all commits on the current branch, and let you name your output file

--- gotit.rb
+++ gotit.rb
@@ -2,20 +2,24 @@ require 'watir-webdriver'
 require 'RMagick'
 include Magick
 
+def human_sort (items)
+  items.sort_by { |item| item.to_s.split(/(\d+)/).map { |e| [e.to_i, e] } }
+end
 #
 # Create a collection of snapshots at commit time for 
 #
-
+output_file = ARGV[1].nil? ?  "animation" : ARGV[1]
 browser = Watir::Browser.new
 branch = `git branch | sed -n '/\* /s///p'`
 shas = `git rev-list #{branch}`
-shas = shas.split("\n").reverse.last(5)
+shas = shas.split("\n").reverse
 temp = 'tmp' + Time.now.to_i.to_s
 `mkdir #{temp}`
-
+a = []
 shas.each_with_index do |sha, index|
   `git checkout #{sha}`
   browser.goto ARGV[0]
+  sleep(1)
   browser.screenshot.save(temp + '/' + index.to_s + '.png')
 end
 
@@ -23,9 +27,9 @@ end
 browser.close
 
 Dir.chdir("#{temp}") do
-  animation = ImageList.new(*Dir["*.png"])
-  animation.delay = 1
-  animation.write("animated.gif")
+  animation = ImageList.new(*human_sort(Dir["*.png"]))
+  animation.delay = 100
+  animation.write("#{output_file}.gif")
   `mv *.gif ..`
 end
 
